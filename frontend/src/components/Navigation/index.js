15b5a546af0a6ba519e85a72ccf700f84e858dfd
Kevin Pravia <keserrano00@gmail.com>
2022-03-05 13:59:06 -0700

    heroku add ons

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
+import logo from './solo-project-2-logo.png'
 
 function Navigation({ isLoaded }){
+  const [searchTerm, setSearchTerm] = useState('');
   const sessionUser = useSelector(state => state.session.user);
 
   let sessionLinks;
@@ -23,11 +25,26 @@ function Navigation({ isLoaded }){
     );
   }
 
+  function handleSearch(event) {
+    event.preventDefault();
+  }
+
   return (
     <ul id='navbar'>
       <li >
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/"><img src={logo} alt={logo} id='logo'/></NavLink>
       </li>
+      <div id='search-bar'>
+        <form onSubmit={handleSearch} id='search-bar'>
+          <input type='search' 
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)} 
+          placeholder='Search...'
+          id='search-bar-input'
+          />
+          <button id='submit-search-button' type='submit'><i className="fa fa-search icon" id='search-icon' aria-hidden="true"></i></button>
+        </form>
+      </div>
       <div id='not-home-links'>
         {isLoaded && sessionLinks}
       </div>
