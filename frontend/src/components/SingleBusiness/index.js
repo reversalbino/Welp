44c436e93138ee53a1086e2804267baea77e8408
Kevin Pravia Mayorga <keserrano00@gmail.com>
2022-03-13 23:26:15 -0600

    add styling to single business page

--- frontend/src/components/SingleBusiness/index.js
+++ frontend/src/components/SingleBusiness/index.js
@@ -29,28 +29,29 @@ function SingleBusiness({ businesses }) {
     <p id='click-to-see-business'>{`<-- Click on a business to see it`}</p>
     :
     <div id='single-business-page'>
-      <p id='single-business-title'>{businessSelected.title}</p>
-      <img id='main-business-image' src={businessSelected.Pictures[0].url} alt={businessSelected.title} />
-      {(user && businessSelected.ownerId === user.id) &&
-        <div id='edit-and-delete-buttons'>
-          <button onClick={deleteBusiness} id='log-in-button'>Delete Business</button>
-          <EditBusinessModal id={id} currentValues={curr}/>
-        </div>
-      }
-      {/* <p id='single-business-address'>{businessSelected.address}, {businessSelected.city}, {businessSelected.state}, {businessSelected.zipCode}</p> */}
-      {/* https://www.google.com/maps/place/3530+Feulner+Dr,+Salt+Lake+City,+UT+84128/ */}
-      <a
-      href={`https://www.google.com/maps/search/${businessSelected.address.split(' ').join('+')},+${businessSelected.city.split(' ').join('+')},+${businessSelected.state.split(' ').join('+')},+${businessSelected.zipCode}`}
-      target='_blank'
-      rel="noreferrer"
-      className='address-link'
-      >
-        {businessSelected.address}, {businessSelected.city}, {businessSelected.state}, {businessSelected.zipCode}
-      </a>
-      <p id='single-business-description'>{businessSelected.description}</p>
+      <div id='business-page-without-reviews'>
+        <p id='single-business-title'>{businessSelected.title}</p>
+        <img id='main-business-image' src={businessSelected.Pictures[0].url} alt={businessSelected.title} />
+        {(user && businessSelected.ownerId === user.id) &&
+          <div id='edit-and-delete-buttons'>
+            <button onClick={deleteBusiness} id='log-in-button'>Delete Business</button>
+            <EditBusinessModal id={id} currentValues={curr}/>
+          </div>
+        }
+        {/* <p id='single-business-address'>{businessSelected.address}, {businessSelected.city}, {businessSelected.state}, {businessSelected.zipCode}</p> */}
+        {/* https://www.google.com/maps/place/3530+Feulner+Dr,+Salt+Lake+City,+UT+84128/ */}
+        <a
+        href={`https://www.google.com/maps/search/${businessSelected.address.split(' ').join('+')},+${businessSelected.city.split(' ').join('+')},+${businessSelected.state.split(' ').join('+')},+${businessSelected.zipCode}`}
+        target='_blank'
+        rel="noreferrer"
+        className='address-link'
+        >
+          {businessSelected.address}, {businessSelected.city}, {businessSelected.state}, {businessSelected.zipCode}
+        </a>
+        <p id='single-business-description'>{businessSelected.description}</p>
 
-      <hr />
-
+        <hr />
+      </div>
       <Reviews id={id} />
     </div>
   );
